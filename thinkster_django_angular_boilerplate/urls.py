2e957aa796e01f8acdc1cb49c9221a9896def7a7
ccjoness <chris.charles.jones@gmail.com>
2015-03-17 14:09:19 -0700

    added register and login python code

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from rest_framework_nested import routers
 from authentication.views import AccountViewSet, LoginView
-from thinkster_django_angular_boilerplate.views import IndexView,
+from thinkster_django_angular_boilerplate.views import IndexView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
